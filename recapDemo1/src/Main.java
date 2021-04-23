6409a6448907d1a3d182b8057028a554f770d881
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2021-04-23 04:18:35 +0300

    Add files via upload

---
+++ recapDemo1/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sayi1 = 20;
+        int sayi2 =25;
+        int sayi3= 2;
+        int enBuyuk = sayi1;
+        if(enBuyuk<sayi2){
+            System.out.println("EN BÜYÜK SAYI:"+sayi2);
+        }
+        if(enBuyuk<sayi3){
+            System.out.println("EN BÜYÜK SAYI :"+sayi3);
+        }
+
+
+    }
+}
