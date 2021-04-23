a5985985b556bab6a71a224b06309723780ef638
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2021-04-23 04:19:10 +0300

    Add files via upload

---
+++ variables/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    //camel casing
+    public static void main(String[] args) {
+        int ogrenciSayisi = 10;
+        String mesaj  = "Öğrenci Sayısı : ";
+
+
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+        System.out.println(mesaj +ogrenciSayisi);
+
+    }
+}
