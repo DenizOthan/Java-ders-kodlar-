97df31e0df82ef0016adfc69d7932258c5feb428
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2021-04-23 04:17:51 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	double sayi = 12.5;
+	sayi = -129;
+	String sehir="Ankara";
+	char karakteR ='A';
+	boolean dogruMu = false;
+    }
+}
