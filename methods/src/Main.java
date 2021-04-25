b9332020ee7732a1edd02e147eef286d2436f58b
DenizOthan <56774747+DenizOthan@users.noreply.github.com>
2021-04-26 02:21:01 +0300

    Add files via upload

---
+++ methods/src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+        sayiBulmaca();
+        sayiBulmaca();
+        sayiBulmaca();
+
+    }//camel casing
+    public static void sayiBulmaca(){
+        int[] sayilar = new int[]{1,2,5,7,9,0};
+        int aranacak = 6;
+        boolean varMi=false;
+
+        for(int sayi :sayilar){
+            if(sayi==aranacak){
+                varMi = true;
+                break;
+            }
+        }
+        if(varMi){
+       mesajVer("Sayı mevcuttur." +aranacak);
+        }else{
+            mesajVer("Sayı mevcut değildir." +aranacak);
+        }
+    }
+    public static void mesajVer(String mesaj){         //Parametreli method
+        System.out.println(mesaj);
+
+    }
+}
